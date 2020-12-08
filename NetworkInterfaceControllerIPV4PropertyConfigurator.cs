5818b757c49ea1aec29fafb6c7d19814ca868183
Frex Cuadillera <fcuadillera@gmail.com>
2020-12-09 01:18:31 +0800

    Create NetworkInterfaceControllerIPV4PropertyConfigurator.cs

---
+++ NetworkInterfaceControllerIPV4PropertyConfigurator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace culs_library
+{
+    public class NetworkInterfaceControllerIPV4PropertyConfigurator
+    {
+        public static void SetStaticIPV4LocalAddress(String interfaceName, String ip, String mask, String gateway)
+        {
+            executeCommand("/c netsh interface ip set address \"" + interfaceName + "\" static " + ip + " " + mask + " " + gateway);
+        }
+
+        public static void SetDHCPIPV4LocalAddress(String interfaceName)
+        {
+            executeCommand("/c netsh interface ip set address \"" + interfaceName + "\" dhcp ");
+        }
+
+        public static void executeCommand(String command)
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo("cmd.exe");
+                psi.UseShellExecute = true;
+                psi.WindowStyle = ProcessWindowStyle.Hidden;
+                psi.Verb = "runas";
+                psi.Arguments = command;
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
