41a34d9cb5e50409e939c21911ad123505bbbaec
Frex Cuadillera <fcuadillera@gmail.com>
2020-12-09 02:46:20 +0800

    changed ExecuteCommand access modifier to private

--- NetworkInterfaceControllerIPV4PropertyConfigurator.cs
+++ NetworkInterfaceControllerIPV4PropertyConfigurator.cs
@@ -5,17 +5,18 @@ namespace culs_library
 {
     public class NetworkInterfaceControllerIPV4PropertyConfigurator
     {
+
         public static void SetStaticIPV4LocalAddress(String interfaceName, String ip, String mask, String gateway)
         {
-            executeCommand("/c netsh interface ip set address \"" + interfaceName + "\" static " + ip + " " + mask + " " + gateway);
+            ExecuteCommand("/c netsh interface ip set address \"" + interfaceName + "\" static " + ip + " " + mask + " " + gateway);
         }
 
         public static void SetDHCPIPV4LocalAddress(String interfaceName)
         {
-            executeCommand("/c netsh interface ip set address \"" + interfaceName + "\" dhcp ");
+            ExecuteCommand("/c netsh interface ip set address \"" + interfaceName + "\" dhcp ");
         }
 
-        public static void executeCommand(String command)
+        private static void ExecuteCommand(String command)
         {
             try
             {
